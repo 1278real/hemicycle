42e3afb9c5c634c4c73399e925e919a78004d174
Alexis FREMONT <contact@1-2-7-8.tv>
2022-07-04 18:19:46 +0200

    Update drawHemicycle.dart

--- lib/attic/drawHemicycle.dart
+++ lib/attic/drawHemicycle.dart
@@ -172,8 +172,7 @@ class _DrawHemicycleState extends State<DrawHemicycle> {
                                       math.sin(
                                           assemblyAngle / 360 * 2 * math.pi))) /
                               2)) *
-                      ((legendRows * (groupSectors != null ? 0.2 : 0.3)) +
-                          (withTitle ? 0.2 : 0)),
+                      ((legendRows * 0.2) + (withTitle ? 0.2 : 0)),
                   child: Column(
                     children: [
                       if (withTitle && title != null)
@@ -236,7 +235,7 @@ class _DrawHemicycleState extends State<DrawHemicycle> {
                                       math.sin(
                                           assemblyAngle / 360 * 2 * math.pi))) /
                               2)) *
-                      ((legendRows * 0.15) + (withTitle ? 0.2 : 0)),
+                      ((legendRows * 0.25) + (withTitle ? 0.2 : 0)),
                   child: Column(
                     children: [
                       if (withTitle && title != null)
